a03301fb8ea7b7bff8f6124ceadbbb5c93d0efc9
ausin107 <nguyenphuqui396@gmail.com>
2024-04-08 21:17:39 +0700

    add new site

--- app/page.tsx
+++ app/page.tsx
@@ -10,17 +10,21 @@ function Home() {
     <main className='lg:h-auto h-fit md:h-screen min-h-screen flex flex-col items-center lg:pt-32 md:pt-40 pt-24 px-5 bg-gradient-to-b from-black to-[#121c1c]'>
       <Suspense>
         <WelcomeBox />
-        <ConnectBox />
       </Suspense>
+      <ConnectBox />
       <div className='flex flex-col items-center justify-center mt-8 md:mb-10 mb-5'>
         <h5 className='mb-8 text-lg text-secondary'>Try our dapp in</h5>
         <CountDown targetDate='2024-04-27' />
       </div>
       <div className='absolute bottom-0 w-full h-[4.5rem] flex items-center justify-end md:px-10 px-5'>
-        <Link href='/privacy' className='hover:text-primary md:mr-10 mr-5 md:text-base text-sm text-secondary'>
+        <Link
+          href='https://expandingbrain.ai/privacy.php'
+          className='hover:text-primary md:mr-10 mr-5 md:text-base text-sm text-secondary'>
           Privacy
         </Link>
-        <Link href='/terms' className='hover:text-primary md:text-base text-sm text-secondary'>
+        <Link
+          href='https://expandingbrain.ai/terms-and-conditions.php'
+          className='hover:text-primary md:text-base text-sm text-secondary'>
           Terms and Conditions
         </Link>
       </div>
--- components/ConnectBox.tsx
+++ components/ConnectBox.tsx
@@ -1,14 +1,12 @@
-'use client'
 import { sendTwitterConversion } from '@/utils/twitterTracking'
 import { ConnectButton } from '@rainbow-me/rainbowkit'
-import { useSearchParams } from 'next/navigation'
 import { twMerge } from 'tailwind-merge'
 function ConnectBox() {
-  const searchParams = useSearchParams()
-  const action = searchParams.get('action') || 'Connect Your Wallet to Use the dAPP'
   return (
     <div className=''>
-      <h5 className='text-secondary lg:text-2xl md:text-3xl text-xl mb-8 text-center'>{action}</h5>
+      <h5 className='text-secondary lg:text-2xl md:text-3xl text-xl mb-8 text-center'>
+        Connect Your Wallet to Use the dAPP
+      </h5>
       <div className={twMerge('flex items-center flex-col flex-wrap')}>
         <ConnectButton.Custom>
           {({ account, chain, openConnectModal, mounted, openAccountModal }) => {
@@ -36,7 +34,7 @@ function ConnectBox() {
                         className={twMerge(
                           'md:flex-none py-5 xl:w-[28rem] lg:w-80 md:w-80 w-56 border-4 border-[#2422e5] rounded-lg bg-[#2422e5] hover:scale-105 text-secondary font-bold xl:text-2xl lg:text-lg md:text-xl text-base uppercase'
                         )}>
-                        Connect wallet
+                        Connect Wallet
                       </button>
                     )
                   }
