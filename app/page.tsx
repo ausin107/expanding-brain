48f64b2a0ce1b724a0a8f944a2c6e5dc61bbc169
ausin107 <nguyenphuqui396@gmail.com>
2024-05-26 10:43:06 +0700

    update count query string

--- app/page.tsx
+++ app/page.tsx
@@ -4,18 +4,21 @@ import { Suspense } from 'react'
 import WelcomeBox from '@/components/WelcomeBox'
 import CountDown from '@/components/CountDown'
 import ConnectBox from '@/components/ConnectBox'
+import { useSearchParams } from 'next/navigation'
 
 function Home() {
+  const searchParam = useSearchParams()
+  const countQueryString = searchParam.get('count') || 'Try our dapp in'
   return (
     <main className='lg:h-auto h-fit md:h-screen min-h-screen flex flex-col items-center lg:pt-32 md:pt-40 pt-24 px-5 bg-gradient-to-b from-black to-[#121c1c]'>
       <Suspense>
         <WelcomeBox />
         <ConnectBox />
+        <div className='flex flex-col items-center justify-center mt-8 md:mb-10 mb-5'>
+          <h5 className='mb-8 text-lg text-secondary'>{countQueryString}</h5>
+          <CountDown targetDate='2024-06-07' />
+        </div>
       </Suspense>
-      <div className='flex flex-col items-center justify-center mt-8 md:mb-10 mb-5'>
-        <h5 className='mb-8 text-lg text-secondary'>Try our dapp in</h5>
-        <CountDown targetDate='2024-06-07' />
-      </div>
       <div className='absolute bottom-0 w-full h-[4.5rem] flex items-center justify-end md:px-10 px-5'>
         <Link href='/privacy' className='hover:text-primary md:mr-10 mr-5 md:text-base text-sm text-secondary'>
           Privacy
