64249acc48fb0b3812ba9ef58b3fe0c5e9c4d5cd
ausin107 <nguyenphuqui396@gmail.com>
2024-05-26 10:43:51 +0700

    fix searchParam bug

--- app/page.tsx
+++ app/page.tsx
@@ -14,11 +14,11 @@ function Home() {
       <Suspense>
         <WelcomeBox />
         <ConnectBox />
+        <div className='flex flex-col items-center justify-center mt-8 md:mb-10 mb-5'>
+          <h5 className='mb-8 text-lg text-secondary'>{countParam}</h5>
+          <CountDown targetDate='2024-06-07' />
+        </div>
       </Suspense>
-      <div className='flex flex-col items-center justify-center mt-8 md:mb-10 mb-5'>
-        <h5 className='mb-8 text-lg text-secondary'>{countParam}</h5>
-        <CountDown targetDate='2024-06-07' />
-      </div>
       <div className='absolute bottom-0 w-full h-[4.5rem] flex items-center justify-end md:px-10 px-5'>
         <Link
           href='https://expandingbrain.ai/privacy.php'
