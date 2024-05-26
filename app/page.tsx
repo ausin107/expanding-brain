5e15dee1558d6c1e8c9a84893f525a775422aca5
ausin107 <nguyenphuqui396@gmail.com>
2024-05-26 10:49:24 +0700

    split component, fix searchParam bug

--- app/page.tsx
+++ app/page.tsx
@@ -2,22 +2,16 @@
 import Link from 'next/link'
 import { Suspense } from 'react'
 import WelcomeBox from '@/components/WelcomeBox'
-import CountDown from '@/components/CountDown'
 import ConnectBox from '@/components/ConnectBox'
-import { useSearchParams } from 'next/navigation'
+import CountDownBox from '@/components/CountDownBox'
 
 function Home() {
-  const searchParams = useSearchParams()
-  const countParam = searchParams.get('count') || 'Try our dapp in'
   return (
     <main className='lg:h-auto h-fit md:h-screen min-h-screen flex flex-col items-center lg:pt-32 md:pt-40 pt-24 px-5 bg-gradient-to-b from-black to-[#121c1c]'>
       <Suspense>
         <WelcomeBox />
         <ConnectBox />
-        <div className='flex flex-col items-center justify-center mt-8 md:mb-10 mb-5'>
-          <h5 className='mb-8 text-lg text-secondary'>{countParam}</h5>
-          <CountDown targetDate='2024-06-07' />
-        </div>
+        <CountDownBox />
       </Suspense>
       <div className='absolute bottom-0 w-full h-[4.5rem] flex items-center justify-end md:px-10 px-5'>
         <Link
